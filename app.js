971b2f83955ae867c44f231630fe95bb2a6fc7ca
Ilnur Gataullin <i-gataullin@yandex-team.ru>
2018-11-26 17:09:15 +0300

    try to add sw

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use(sassMiddleware({
         indentedSyntax: false, // true = .sass and false = .scss
         sourceMap: true,
     }));
+app.use('/sw.js', express.static(path.join(__dirname, 'src/sw.js')));
 app.use('/src', express.static(path.join(__dirname, 'src')));
 app.use('/build', express.static(path.join(__dirname, 'build')));
 
