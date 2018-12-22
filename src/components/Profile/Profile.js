6430785cbd6b68acd445d588e0e356f9d1344bc6
Vladimir Atamanov <viewsharp@yandex.ru>
2018-12-22 17:17:00 +0300

    hf

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -2,9 +2,14 @@ import Component from "../Component.js";
 import VirtualDOM from "../VirtualDOM.js";
 import renderProfile from "./Profile.pug.js";
 import {NO_AVATAR_PATH, IMAGES_PATH} from "../../constants.js";
+import {BOT_AVATAR_PATH} from "../../constants";
 
 export default class Profile extends Component {
     constructor(props = {}) {
+        if (props.avatar !== BOT_AVATAR_PATH) {
+            props.avatar = IMAGES_PATH + props.avatar;
+        }
+
         super(VirtualDOM.createElementByHtml(renderProfile({
             avatar: props.avatar ? IMAGES_PATH + props.avatar : NO_AVATAR_PATH,
             name: props.name || ''
